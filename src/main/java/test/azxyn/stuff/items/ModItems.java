9cbb146bda34d7d20b3edfc59b2ca85a0260c93c
Azaan <azaanaltf4@gmail.com>
2025-03-31 22:00:08 +0500

    Added Custom Items

--- src/main/java/test/azxyn/stuff/items/ModItems.java
+++ src/main/java/test/azxyn/stuff/items/ModItems.java
@@ -3,24 +3,29 @@ package test.azxyn.stuff.items;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import test.azxyn.stuff.Axyzn;
 
+import java.util.function.Function;
+
 public class ModItems {
 
-    public static final Item HEART_LIFESTEAL = registerItem("heart_lifesteal", new Item(new Item.Settings()));
+    public static final Item HEART_LIFESTEAL = registeritem("heart_lifesteal", Item::new, new Item.Settings());
+    public static final Item REVIVE_BEACON = registeritem("revive_beacon", Item::new, new Item.Settings());
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(Axyzn.MOD_ID, name), item);
+    public static Item registeritem(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        final RegistryKey<Item> regkey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Axyzn.MOD_ID, path));
+        return Items.register(regkey, factory, settings);
     }
-
     public static void registerModItems() {
         Axyzn.LOGGER.info("registering items for" + Axyzn.MOD_ID);
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> {
             entries.add(HEART_LIFESTEAL);
+            entries.add(REVIVE_BEACON);
         });
     }
 
